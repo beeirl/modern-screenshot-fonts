16705a4d198f19a6efd662b3246c35abd24c4e1b
beeirl <christoph@5head.org>
2025-03-06 00:07:17 +0100

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { domToPng } from 'modern-screenshot';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div id="test">Hello world!</div>
+      <div id="node">Hello world!</div>
       <button
         onClick={() => {
-          domToPng(document.getElementById('test')!).then((dataUrl) => {
+          domToPng(document.getElementById('node')!).then((dataUrl) => {
             const link = document.createElement('a');
             link.download = 'screenshot.png';
             link.href = dataUrl;
@@ -29,19 +15,8 @@ function App() {
           });
         }}
       >
-        Test
+        Screenshot
       </button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
